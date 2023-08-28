4248c4e2bffb96c0ea6b0d46f0367e1d3e9d6d62
ArcticWarmth <ArcticWarmthDev@protonmail.com>
2023-08-27 21:22:53 -0500

    Migrate to use config

--- src/main/java/net/arcticwarmth/noend/noend/func/RespawnPosFinder.java
+++ src/main/java/net/arcticwarmth/noend/noend/func/RespawnPosFinder.java
@@ -1,20 +1,23 @@
 package net.arcticwarmth.noend.noend.func;
 
+import net.arcticwarmth.noend.noend.ConfigMgr;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
+
+import static net.arcticwarmth.noend.noend.DisableEnd.CONFIG;
+
 public class RespawnPosFinder {
-    static String[] KnownEndings = {"bed", "respawn_anchor"};
+
+    private static String[] respawnBlocks = CONFIG.respawnblocks();
     public static int[] FindRespawnPos(int X, int Y, int Z, World world) {
-        for (int i = 0; i < KnownEndings.length; i++) {
-            if(world.getBlockState(new BlockPos(X, Y, Z)).getBlock().getName().toString().split("'")[1].endsWith(KnownEndings[i])) {
+
+        for (int i = 0; i < respawnBlocks.length; i++) {
+            if(Registries.BLOCK.getId(world.getBlockState(new BlockPos(X, Y, Z)).getBlock()).toString().equals(respawnBlocks[i])) {
                 return new int[]{X, Y, Z};
             }
         }
-        int[] pos = new int[3];
-        pos[0] = world.getSpawnPos().getX();
-        pos[1] = world.getSpawnPos().getY();
-        pos[2] = world.getSpawnPos().getZ();
+        int[] pos = new int[]{world.getSpawnPos().getX(), world.getSpawnPos().getY(), world.getSpawnPos().getZ()};
         return pos;
     }
 }
