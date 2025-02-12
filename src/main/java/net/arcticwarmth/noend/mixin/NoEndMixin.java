1166499f39055b63cbdc500efb5aac17e9fefc0b
ArcticWarmth <ArcticWarmthDev@protonmail.com>
2025-02-12 12:55:00 -0600

    feat: rework collision logic to cancel the mixin because I was vastly overthinking everything when I first wrote it

--- src/main/java/net/arcticwarmth/noend/mixin/NoEndMixin.java
+++ src/main/java/net/arcticwarmth/noend/mixin/NoEndMixin.java
@@ -21,42 +21,11 @@ import static net.arcticwarmth.noend.noend.func.RespawnPosFinder.FindRespawnPos;
 
 @Mixin(EndPortalBlock.class)
 public class NoEndMixin {
-    @Inject(method = "onEntityCollision", at = @At("HEAD"))
+    @Inject(method = "onEntityCollision", at = @At("HEAD"), cancellable = true)
     private void injectOnEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci) {
 
-        if (world.getGameRules().getBoolean(net.arcticwarmth.noend.noend.server.DisableEndServer.DISABLE_END) && world instanceof ServerWorld && entity.canUsePortals(true) && !entity.hasVehicle() && !entity.hasPassengers() && entity.getWorld().getRegistryKey() != World.END) {
-
-            if(entity.isPlayer()) {
-
-                if(entity.getServer().getPlayerManager().getPlayer(entity.getUuid()).getSpawnPointPosition() != null) {
-                    System.out.println(entity.getServer().getPlayerManager().getPlayer(entity.getUuid()).getSpawnPointPosition());
-                    double spawnx = entity.getServer().getPlayerManager().getPlayer(entity.getUuid()).getSpawnPointPosition().getX();
-                    double spawnz = entity.getServer().getPlayerManager().getPlayer(entity.getUuid()).getSpawnPointPosition().getZ();
-                    double spawnY = entity.getServer().getPlayerManager().getPlayer(entity.getUuid()).getSpawnPointPosition().getY();
-                    RespawnLocation rL = FindRespawnPos((int)spawnx, (int)spawnY, (int)spawnz, entity.getWorld());
-                    int[] tpos = rL.position;
-                    Set<PositionFlag> flags = new HashSet<>();
-                    entity.teleport(rL.world, tpos[0], tpos[1], tpos[2], flags, entity.getYaw(), entity.getPitch());
-                } else {
-                    //TODO: Add no bed found message
-                    double spawnx = entity.getWorld().getSpawnPos().getX();
-                    double spawnz = entity.getWorld().getSpawnPos().getZ();
-                    double spawnY = entity.getWorld().getSpawnPos().getY();
-                    Set<PositionFlag> flags = new HashSet<>();
-                    entity.teleport((net.minecraft.server.world.ServerWorld) world, spawnx, spawnY, spawnz, flags, entity.getYaw(), entity.getPitch());
-                }
-
-
-                System.out.println("Respawned");
-            } else {
-
-                double spawnx = entity.getWorld().getSpawnPos().getX();
-                double spawnz = entity.getWorld().getSpawnPos().getZ();
-                double spawnY = entity.getWorld().getSpawnPos().getY();
-                Set<PositionFlag> flags = new HashSet<>();
-                entity.teleport((net.minecraft.server.world.ServerWorld) world, spawnx, spawnY, spawnz, flags, entity.getYaw(), entity.getPitch());
-                System.out.println("Teleported");
-            }
+        if (world.getServer().getGameRules().getBoolean(net.arcticwarmth.noend.noend.server.DisableEndServer.DISABLE_END) && world instanceof ServerWorld && entity.canUsePortals(true) && !entity.hasVehicle() && !entity.hasPassengers() && entity.getWorld().getRegistryKey() != World.END) {
+            ci.cancel();
         }
     }
 
